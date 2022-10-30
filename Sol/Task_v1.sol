1e2cfd2bbc2ebc8d1a616c8ce35d776f3735a968
Spiros Grigoratos <spiros@bitloops.com>
2022-10-30 21:56:22 +0200

    added to states

--- Sol/Task_v1.sol
+++ Sol/Task_v1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
-//taskID to be bytes32 instead of address
+//taskID to be byted32 instead of address
 
 
 contract Task{
@@ -10,20 +10,21 @@ contract Task{
         Created,
         Cancelled,
         Active,
-        Complete,
+        Completed,
         Invalid
     }
 
     address payable internal provider;
     address payable internal client;
-    address internal taskID; //by auction contract  //can be 
+    address internal taskID; //by auction contract  //can be bytes32
     
-    uint internal payment;   //float at front-end
-    uint internal collateral;   //float at front-end
-    uint internal duration;  //contract duration, in ms
-    uint internal activateDate = block.timestamp;   //contract activation date, in ms since epoch
-    uint internal deadline;  //contract deadline, in ms since epoch
-    uint internal timeResultReceived;    //time of received result, in ms since epoch   
+    uint internal payment;   //can be float at front-end
+    uint internal collateral;   //can be float at front-end
+    uint internal duration;  //contract duration, in sec
+    uint internal activateDate;   //contract activation date, in sec since epoch
+
+    uint internal timeResultProvided;     //time of result given by provider, in sec since epoch
+    uint internal timeResultReceived;    //time of received result, in sec since epoch   
 
     State internal state;   //state of contract
 
@@ -37,7 +38,7 @@ contract Task{
     event TaskCompleted(address taskID);
     event TaskCancelled(address taskID);
     event TaskInvalidated(address taskID);
-
+    event TransferMade(address Address, uint Amount);
 
     //Modifiers 
     modifier requiresClient() {
@@ -87,33 +88,83 @@ contract Task{
         state = State.Created;
     }
 
+    //  States to be called by smart contract or client/provider? 
+
     //Cancel
     //State -> Cancel
     //refunds payment to client
     //can be called only by client and only if contract hasnt been activated by provider
-    function cancel() public requiresClient requiresState(State.Created)
+
+    // - no requiresClient so that it can be tested
+    // function cancelContract() public payable requiresClient requiresState(State.Created) 
+    function cancelContract() public payable requiresState(State.Created)
     {
         state = State.Cancelled;
-        payable(msg.sender).transfer(address(this).balance);
-
+        // payable(msg.sender).transfer(address(this).balance);
+        // sender is client, balance is payment, the contract is not activated so there is not collateral
+        client.transfer(payment);
+        emit TransferMade(client,payment);
         emit TaskCancelled(taskID);
     }
 
     //Invalidate
     ///State -> Invalidate
-    //can be called only by client and only if contract is activated
+    //can be called only by client and only if contract is activated  // no by client but auto?
     //if time has passed and the task is not comleted by the provider
     //transfers payment and collateral to client
-    function invalidate() public requiresClient requiresState(State.Active)
+    
+    // - no requiresClient so that it can be tested
+    // function invalidateContract() public payable requiresClient requiresState(State.Active)
+    function invalidateContract() public payable requiresState(State.Active)
     {
         require(
-            (block.timestamp * 1000 >  activateDate + duration),
-            "...."
+            (block.timestamp >  activateDate + duration),
+            "Time has not expired"
         );
         state = State.Invalid;
         //add transfer of payment
 
+        // payable(msg.sender).transfer(address(this).balance);
+        uint payAmount = payment + collateral;
+        client.transfer(payAmount);
+        emit TransferMade(client, payAmount);
         emit TaskInvalidated(taskID);
+    }
+
+    // Activate
+    // State -> Activated
+    // can be called only by provider to start the process
+
+    // - no requiresProvider so that it can be tested
+    // function activateContract() public requiresProvider requiresState(State.Created)
+    function activateContract() public requiresState(State.Created)
+    {
+        activateDate = block.timestamp;
+        state = State.Active;
+    }
+
+
+    // Complete
+    // State -> Completed
+    // can be called only by provider when the computation is over
+
+    // - no requiresProvider so that it can be tested
+    // function completeContract() public payable requiresProvider requiresState(State.Active)
+    function completeContract() public payable requiresState(State.Active)
+    {
+        state = State.Completed;
+        // payable(msg.sender).transfer(address(this).balance);
+        uint payAmount = payment + collateral;
+        if (InTime() && CorrectVerification()){
+            provider.transfer(payment+collateral);
+            emit TransferMade(provider, payAmount);
+        }
+        else {
+            emit TransferMade(client, payAmount);
+            client.transfer(payment+collateral);
+        }
+        
+        emit TaskCompleted(taskID);
     }
 
     
@@ -122,9 +173,10 @@ contract Task{
         providerVerification = ver;
     }
 
-    //could also be this.time
-    function setTimeResultReceived (uint _timeReceived) public{
-        timeResultReceived = _timeReceived;
+    // times can be compared too
+    function setTimeResultReceived (uint _timeReceivedProvider) public{
+        timeResultReceived = block.timestamp;
+        timeResultProvided = _timeReceivedProvider;
     }
 
     function setCode(string memory _code) public{
@@ -167,13 +219,12 @@ contract Task{
         return activateDate;
     }
 
-    function getDeadline() public view returns (uint)
-    {
-        return deadline;
-    }
-
     function getTimeResultReceived() public view returns (uint){
         return timeResultReceived;
+    }
+
+     function getTimeResultProvided() public view returns (uint){
+        return timeResultProvided;
     }
 
     function getCode() public view returns (string memory){
@@ -182,32 +233,33 @@ contract Task{
 
     //TODO: Add state transitions
     // State: Created->0, Cancelled->1, Active->2, Complete->3, Invalid->4
-    function getStatus() public view returns (int8)
+    function getState() public view returns (State)
     {
-        if (state == State.Created)
-        {
-            return 0;
-        }
-        else if (state == State.Cancelled)
-        {
-            return 1;
-        }
-        else if (state == State.Active)
-        {
-            return 2;
-        }
-        else if (state == State.Complete)
-        {
-            return 3;
-        }
-        else if (state == State.Invalid)
-        {
-            return 4;
-        }
-        else
-        {
-            return -1;
-        }
+        return state;
+        // if (state == State.Created)
+        // {
+        //     return 0;
+        // }
+        // else if (state == State.Cancelled)
+        // {
+        //     return 1;
+        // }
+        // else if (state == State.Active)
+        // {
+        //     return 2;
+        // }
+        // else if (state == State.Completed)
+        // {
+        //     return 3;
+        // }
+        // else if (state == State.Invalid)
+        // {
+        //     return 4;
+        // }
+        // else
+        // {
+        //     return -1;
+        // }
     }
    
    function getClientVerification() public view returns (string memory){
@@ -216,6 +268,10 @@ contract Task{
 
     function getProviderVerification() public view returns (string memory){
         return providerVerification;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
     //Functions
