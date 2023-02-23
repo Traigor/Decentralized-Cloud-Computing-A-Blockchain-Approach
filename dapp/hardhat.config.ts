4c307bbee6c56a84d0d5992b7ee8ae732d6c959d
Spiros Grigoratos <spgrigoratos@gmail.com>
2023-02-23 17:20:05 +0200

    gas-report

--- dapp/hardhat.config.ts
+++ dapp/hardhat.config.ts
@@ -16,6 +16,13 @@ const config: HardhatUserConfig = {
       1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
   },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
 
 export default config;
