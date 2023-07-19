258c8ad91f068acca9bbcf0b8e208eb0ca65bb92
Spiros Grigoratos <spgrigoratos@gmail.com>
2023-07-19 16:30:06 +0300

    cleaned old files

--- Python/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 ]
-  then
-    docker build -t test .
-  else
-    docker build -t test --build-arg seed="$*" .
-fi
-docker run test
--- Python/script2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python3 maker.py
-python3 script.py $seed
